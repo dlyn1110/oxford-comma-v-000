4ff2142422eb7b3d174ea6b45639c24e53c20f2c
Dani Lyn <dlyn1110@gmail.com>
2018-04-10 15:43:00 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+if (array.count == 1)
+  return array.join
+elsif (array.count == 2)
+  return arra.join(" and ")
+  e
+end
 
+#def oxford_comma(array)
+  array = ["kiwi","durian"]
+  array.join(" and ")
 end
