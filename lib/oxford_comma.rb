5cd9250d58b52ea18340d52a35f9f7ffa6d571e3
Dani Lyn <dlyn1110@gmail.com>
2018-04-10 15:46:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if (array.count == 1)
 elsif (array.count == 2)
   return arra.join(" and ")
 elsif (array.count >= 3 )
-  return array[0..-2].join(",") + (",and ") + array[-1]
+  return array[0..-2].join(",") + ", and " + array[-1]
 else
   return array
 end
